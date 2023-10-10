8a9fdae8bd519c3a3f01d53ef57d0c2922a78623
Code and core <codeandcore@gmail.com>
2023-10-10 19:04:11 +0530

    initial files added

---
+++ swapImg/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+from . import views
+
+urlpatterns = [
+    path('', views.avatarView, name='user-info'),
+    path('success/', views.uploadok, name = 'success'),
+    path('listing/', views.DisplayRegisteredUser, name = 'user-info-listing'),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
